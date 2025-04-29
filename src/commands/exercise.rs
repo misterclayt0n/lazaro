7810c2f4fb3992a1e0a5cc660b9e66c0e8acc136
misterclayt0n <daviarantes13@gmail.com>
2025-04-29 19:15:23 -0300

    Better UX

--- src/commands/exercise.rs
+++ src/commands/exercise.rs
@@ -1,5 +1,5 @@
 use sqlx::SqlitePool;
-
+use colored::Colorize;
 use crate::cli::ExerciseCmd;
 use anyhow::Result;
 
@@ -21,20 +21,24 @@ pub async fn handle(cmd: ExerciseCmd, pool: &SqlitePool) -> Result<()> {
             .await;
 
             match res {
-                Ok(info) if info.rows_affected() == 1 =>  println!("Exercise \"{}\" added", &name),
-                Ok(_) => println!("Exercise \"{}\" was not inserted", &name),
+                Ok(info) if info.rows_affected() == 1 => println!("{} Exercise \"{}\" added", "info:".blue().bold(), &name),
+                Ok(_) => println!("{} Exercise \"{}\" was not inserted", "info:".blue().bold(), &name),
                 Err(sqlx::Error::Database(db_err)) if db_err.code() == Some("2067".into()) => {
                     // 2067 = SQLITE_CONSTRAINT_UNIQUE
                     println!(
-                        "Exercise \"{}\" already exists — use `ex list` to view all exercises",
+                        "{} Exercise \"{}\" already exists — use `ex list` to view all exercises",
+                        "warning:".yellow().bold(),
                         name
                     );
                 }
-                Err(e) => return Err(e.into())
+                Err(e) => {
+                    println!("{} {}", "error:".red().bold(), e.to_string().red());
+                    return Err(e.into());
+                }
             }
         }
         
-        ExerciseCmd::Import { file: _ } => {
+        ExerciseCmd::Import { file } => {
             // read TOML, deserialize into a Vec<struct { name, description, primary_muscle }>
             // then loop and `INSERT OR IGNORE INTO exercises ...`
             todo!("impl exercise import");
